962ae52d542a212edb59365b461d4cd22a1c747b
Luis Caballero <lacb0311@hotmail.com>
2023-04-03 10:28:59 -0500

    Commit con solucion a prueba tecnica de react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+
+function accionar() {
+    const text = document.getElementById("texto").value
+    const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const vocales = ["a", "e", "i", "o", "u"]
+
+    let contadorNumeros = 0
+    let contadorVocales = 0
+
+    const cadena = text.split("")
+    console.log(cadena)
+
+    for (let i = 0; i < cadena.length; i++) {
+      if(vocales.includes(cadena[i])){
+        contadorVocales += 1
+      }
+      // if(numeros.includes(cadena[i])){
+      //   contadorNumeros += 1
+      // }
+    }
+
+    for(var i in numeros){
+        if(cadena.includes(i)){
+          contadorNumeros += 1
+        }
+      }
+
+  alert("La cantidad de numeros es: "+contadorNumeros+"\nLa cantidad de vocales es: "+contadorVocales)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' id='texto'></input>
+      <button id='boton' onClick={accionar}> Aceptar </button>
     </div>
   );
 }
